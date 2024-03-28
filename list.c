147060503b8b938eb24548fe141679f86a669b51
Your Name <you@example.com>
2024-03-28 14:06:58 +0000

    new try

--- list.c
+++ list.c
@@ -29,7 +29,12 @@ Node * createNode(void * data) {
 }
 
 List * createList() {
-     return NULL;
+  //la cual retorna el puntero del tipo de dato List con sus valores inicializados en NULL.
+  List * lista = (List *)malloc(sizeof(List));
+  lista->head = NULL;
+  lista->tail = NULL;
+  lista->current = NULL;
+  return lista;
 }
 
 void * firstList(List * list) {
